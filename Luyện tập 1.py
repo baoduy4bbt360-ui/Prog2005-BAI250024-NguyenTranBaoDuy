cc69382720540c4db7d33b164820fd005861eb1c
Prog2007-BAI250024-Nguyễn Trần Bảo Duy <baoduy4bbt360@gmail.com>
2026-05-18 15:30:14 +0700

    Add files via upload

---
+++ Luyện tập 1.py
@@ -0,0 +1,56 @@
+#Bài 1
+
+so_nguyen = 2
+so_thuc = 3.5
+chuoi = "Hello anh em!"
+print("Bài 1")
+print("Số nguyên:", so_nguyen)
+print("Số thực:", so_thuc )
+print("Chuỗi:", chuoi)
+
+#Bài 2
+pi= float(3.14)
+r= int(5)
+C= 2*pi*r
+print("Bài 2")
+print("Chu vi của hình tròn có r=5 là: ", C)
+
+#Bài 3
+num1= int(3)
+num2= int(6)
+print("Bài 3")
+print("phép tổng: ",num1+num2)
+print("Phép hiệu: ",num1-num2)
+print("Phép tích: ",num1*num2)
+print("Phép thương: ",num1/num2)
+
+#Bài 4
+def sum_two_numbers(a, b):
+    tong= a+b
+    return tong
+print("Bài 4")
+print("kết quả: ",sum_two_numbers(3, 6))
+
+#Bài 5
+name= str("Nguyễn Trần Bảo Duy")
+age= int(19)
+average_score= float(9.5)
+age_next_year= age+1
+doubled_score= average_score*2
+type(name)
+type(age)
+type(average_score)
+type(age_next_year)
+type(doubled_score)
+print("Bài 5")
+print("Họ và tên: ", name)
+print("Tuổi năm nay: ",age)
+print("Điểm trung bình:",average_score)
+print("Tuổi năm tiếp theo: ",age_next_year)
+print("Nhân đôi điểm trung bình: ",doubled_score)
+print(type(name))
+print(type(age))
+print(type(average_score))
+print(type(age_next_year))
+print(type(doubled_score))
+
