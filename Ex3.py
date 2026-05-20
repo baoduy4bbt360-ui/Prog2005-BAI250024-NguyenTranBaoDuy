117427b469efe0e0fdbf8c518280cc6607298b8b
Prog2005-BAI250024-Nguyễn Trần Bảo Duy <baoduy4bbt360@gmail.com>
2026-05-20 17:40:58 +0700

    Delete Ex3.py

--- Ex3.py
+++
@@ -1,9 +0,0 @@
-#Bài 3
-num1= int(3)
-num2= int(6)
-print("Bài 3")
-print("phép tổng: ",num1+num2)
-print("Phép hiệu: ",num1-num2)
-print("Phép tích: ",num1*num2)
-print("Phép thương: ",num1/num2)
-
